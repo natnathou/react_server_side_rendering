e6df7132115b793224489658b6f8291707d3430a
Netanel <ne.zaffran@gmail.com>
2021-03-15 20:46:05 +0200

    first install ts

--- server_side_ts_rendering/webpack.client.js
+++ server_side_ts_rendering/webpack.client.js
@@ -1,25 +1,23 @@
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
-const baseConfig = require('./webpack.base');
 const { merge } = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
 
 const config = {
-  target: 'node',
-  entry: './src/index.ts',
-  externals: [nodeExternals()],
+  entry: './src/client/index.tsx',
 
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
+    filename: 'client.js',
+    path: path.resolve(__dirname, 'public'),
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json'],
   },
+
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: ['ignore-loader'],
+        use: 'ignore-loader',
       },
     ],
   },
